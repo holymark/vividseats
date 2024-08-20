0f85c85bc5be88389dd50fb32e499afc793f3062
mkmark53 <mkmark53@gmail.com>
2024-08-20 03:44:55 +0000

    ,,

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
-// Apify SDK - toolkit for building Apify Actors (Read more at https://docs.apify.com/sdk/js/)
 import { Actor } from 'apify';
-// Crawlee - web scraping and browser automation library (Read more at https://crawlee.dev)
 import { CheerioCrawler, Dataset } from 'crawlee';
-// this is ESM project, and as such, it requires you to specify extensions in your relative imports
-// read more about this here: https://nodejs.org/docs/latest-v18.x/api/esm.html#mandatory-file-extensions
-// note that we need to use `.js` even when inside TS files
-// import { router } from './routes.js';
+
 
 interface Input {
     startUrls: string[];
